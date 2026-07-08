21ff35aff90474bf74deacfb1bf909383478c297
harshwardhans279sharma-ctrl <harshwardhans279sharma@gmail.com>
2026-07-08 14:25:36 +0530

    Day 4: ORM deep dive, Django shell, task detail page with get_object_or_404

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
 	path('',views.home,name='home'),
 	path('about/',views.about,name='about'),
 	path('tasks/', views.task_list, name='task-list'),
-
-	]
+	path('tasks/<int:pk>',views.task_detail,name='task-detail')
+		]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,get_object_or_404
 from .models import Task
+
 def home(request):
     context = {'username': "Harshwardhan",
     'total_tasks' : Task.objects.count()
@@ -17,3 +19,9 @@ def task_list(request):
     }
     return render(request, 'tasks/task_list.html',context)
 
+def task_detail(request,pk):#pk is the primary key and can also be seen as the task_id for viewing the detail about that particular task with the plk or task_id
+    task = get_object_or_404(Task, pk=pk)
+    context={
+    'task' : task,}
+
+    return render(request,'tasks/task_detail.html',context)
