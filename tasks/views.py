33fe59ece21acb73dfb5bf2d9aec726c49f9d17e
harshwardhans279sharma-ctrl <harshwardhans279sharma@gmail.com>
2026-07-15 20:15:09 +0530

    Day 11: Pagination with correct row numbering across pages

--- tasks/views.py
+++ tasks/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 def home(request):
@@ -38,8 +39,12 @@ def task_list(request):
     if status_filter:
         tasks = tasks.filter(status=status_filter)
 
+    paginator = Paginator(tasks,10)
+    page_number = request.GET.get('page',1)
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'tasks': tasks,
+        'page_obj':page_obj,
         'search': search,
         'status_filter': status_filter,
     }
