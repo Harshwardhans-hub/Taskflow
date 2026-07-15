460cd6ee17e24a8b346013263b3a5c21111ecbad
harshwardhans279sharma-ctrl <harshwardhans279sharma@gmail.com>
2026-07-15 22:22:24 +0530

    Day 5: TaskForm, Create Task page, CSRF, form validation, + New Task button
    # Conflicts:
    #	templates/about.html

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 	path('',views.home,name='home'),
 	path('about/',views.about,name='about'),
 	path('tasks/', views.task_list, name='task-list'),
-	path('tasks/<int:pk>',views.task_detail,name='task-detail')
+	path('tasks/<int:pk>/',views.task_detail,name='task-detail'),
+	path('tasks/create/',views.task_create,name='task-create'),
 		]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
-
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
+from .forms import TaskForm
+
 
 def home(request):
     context = {'username': "Harshwardhan",
@@ -25,3 +26,17 @@ def task_detail(request,pk):#pk is the primary key and can also be seen as the t
     'task' : task,}
 
     return render(request,'tasks/task_detail.html',context)
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task-list')
+    else:
+        form = TaskForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'tasks/task_form.html', context)
